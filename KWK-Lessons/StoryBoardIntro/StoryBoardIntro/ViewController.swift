45bea4e3bed9ad49d8f65790ac34aaaf190612e2
IzzyBizzyCoding <87436915+IzzyBizzyCoding@users.noreply.github.com>
2021-07-14 11:37:24 -0400

    1st Version

---
+++ KWK-Lessons/StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by  Scholar on 7/14/21.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+    }
+
+
+}
+
